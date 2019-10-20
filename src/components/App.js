9511a6d9ff04a5ac3a5c7f0de50bbb1539de0bfe
skillup <test@test.com>
2019-10-20 15:47:31 +0300

    vfds

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import Footer from './Footer';
-import Header from './Header';
+import React, { useState } from 'react';
+// import Footer from './Footer';
+// import Header from './Header';
+
+const names = ['vasa', 'pitya', 'alex', 'an'];
+
+const render = names.map((name, i) => <p key={i}>{name}</p>)
 
 function App() {
+
+  const [res, setRes] = useState('hello');
+
+  const show = () => {
+    setRes(res + ' world')
+  }
+
   return (
-    <>
-      <Header />
-      <Footer />
-    </>
+    <div id="app">
+      <h2 onClick={show} >Hello app</h2>
+      {res}
+    </div>
   );
 }
 

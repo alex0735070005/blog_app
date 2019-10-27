7d73e88b309df3b34cbb58b0a53f0d462d7409d4
skillup <test@test.com>
2019-10-27 15:47:06 +0200

    create styles header page footer

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 
 const Home = (props) => {
 
-    const { title, numberPage } = props;
-
     return (
-        <div className="home">
-            <h1>{title}</h1>
-            <h2>{numberPage}</h2>
+        <div className="home">           
+            <h1>Home</h1>
         </div>
     )
 }
